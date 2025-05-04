8c93a5732618a420ea541923058a093553605126
obeginin <obeginin@lihotec.com>
2025-05-04 14:16:54 +0300

    Вывод подзадач в категориях (task_js_2). task_js - так и остался просто категории

--- Routers/tasks.py
+++ Routers/tasks.py
@@ -10,54 +10,64 @@ from Models import Task
 task_router  = APIRouter(prefix="/tasks", tags=["tasks"])
 subtask_router  = APIRouter(prefix="/subtasks", tags=["subtasks"])
 task_js_router = APIRouter(prefix="/js", tags=["js"])
-task_ji_router = APIRouter(prefix="/html", tags=["js"])
+task_ji_router = APIRouter(prefix="/html", tags=["html"])
 templates = Jinja2Templates(directory="templates")
 
+# /tasks
+''' Эндпоинт: Получить список задач (/tasks/api/)'''
+@task_router.get("/api/", response_model=list[TaskRead],summary="Получить список задач")
+def read_all_tasks(db: Session = Depends(get_db)):
+    return task_crud.get_all_tasks(db)
+
+''' Эндпоинт: Получить задачу по id (/tasks/api/{task_id})'''
+@task_router.get("/api/{task_id}", response_model=list[TaskRead],summary="Получить задачу по id")
+def read_tasks_id(task_id: int, db: Session = Depends(get_db)):
+    return task_crud.get_task_id(db, task_id)
+
+# /subtasks
+''' Эндпоинт: Получить список всех столбцов таблицы (/subtasks/api/)'''
+@subtask_router.get("/api/columns", response_model=list[str],summary="Получить список всех столбцов таблицы 'subtasks'")
+def get_task_columns(db: Session = Depends(get_db)):
+    return task_crud.get_columns_of_table(db)
+
+''' Эндпоинт: Получить список всех подзадач (/subtasks/api/)'''
+@subtask_router.get("/api/", response_model=list[SubTaskRead],summary="Получить список всех подзадач")
+def read_all_subtasks(db: Session = Depends(get_db)):
+    return task_crud.get_all_subtasks(db)
+
+''' Эндпоинт: Получить подзадачу по id (/subtasks/api/{subtask_id})'''
+@subtask_router.get("/api/{subtask_id}", response_model=list[SubTaskRead],summary="Получить подзадачу по id")
+def read_subtasks_id(subtask_id: int, db: Session = Depends(get_db)):
+    return task_crud.get_subtasks_id(db, subtask_id)
+
+''' Эндпоинт: Получить подзадачу по TaskID (/subtasks/api/{task_id})'''
+@subtask_router.get("/api/TaskID/{task_id}", response_model=list[SubTaskRead],summary="Получить подзадачу по TaskID")
+def read_subtasks_TaskID(task_id: int, db: Session = Depends(get_db)):
+    return task_crud.get_subtasks_TaskID(db, task_id)
+
+# /subtasks (POST)
+''' Эндпоинт: Добавление подзадачи POST /tasks/api/'''
+@subtask_router.post("/api/", status_code=201,summary="Добавление подзадачи")
+def create_new_subtask(subtask: SubTaskCreate, db: Session = Depends(get_db)):
+    new_id = task_crud.create_subtask(db, subtask)
+    return {"message": "Подзадача успешно добавлена", "SubTaskID": new_id}
+
+
+# /tasks
 '''Подключаем html файл с JavaScript'''
 @task_js_router.get("/", response_class=HTMLResponse)
 def list_tasks_js(request: Request):
     return templates.TemplateResponse("tasks_js.html", {"request": request})
 
+# /subtasks по task_id
+'''@subtask_router.get("/{task_id}", response_model=list[SubTaskRead],summary="Получить подзадачу по TaskID")
+def read_subtasks_TaskID(task_id: int, db: Session = Depends(get_db)):
+    return task_crud.get_subtasks_TaskID(db, task_id)'''
+
+# /html
 '''Подключаем html файл с Jinja2'''
 @task_ji_router.get("/", response_class=HTMLResponse)
 def list_tasks_html(request: Request, db: Session = Depends(get_db)):
     tasks = db.query(Task).all() # передаю модель Task в запрос
     return templates.TemplateResponse("tasks_jinja.html", {"request": request, "tasks": tasks})
 
-''' Эндпоинт: Получить список задач (/tasks/)'''
-@task_router.get("/", response_model=list[TaskRead],summary="Получить список задач")
-def read_all_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_all_tasks(db)
-
-''' Эндпоинт: Получить задачу по id (/tasks/{task_id})'''
-@task_router.get("/{task_id}", response_model=list[TaskRead],summary="Получить задачу по id")
-def read_tasks_id(task_id: int, db: Session = Depends(get_db)):
-    return task_crud.get_task_id(db, task_id)
-
-''' Эндпоинт: Получить список всех столбцов таблицы (/subtasks/)'''
-@subtask_router.get("/columns", response_model=list[str],summary="Получить список всех столбцов таблицы 'subtasks'")
-def get_task_columns(db: Session = Depends(get_db)):
-    return task_crud.get_columns_of_table(db)
-
-''' Эндпоинт: Получить список всех подзадач (/subtasks/)'''
-@subtask_router.get("/", response_model=list[SubTaskRead],summary="Получить список всех подзадач")
-def read_all_subtasks(db: Session = Depends(get_db)):
-    return task_crud.get_all_subtasks(db)
-
-''' Эндпоинт: Получить подзадачу по id (/subtasks/{task_id})'''
-@subtask_router.get("/id/{subtask_id}", response_model=list[SubTaskRead],summary="Получить подзадачу по id")
-def read_subtasks_id(subtask_id: int, db: Session = Depends(get_db)):
-    return task_crud.get_subtasks_id(db, subtask_id)
-
-''' Эндпоинт: Получить подзадачу по TaskID (/subtasks/{task_id})'''
-@subtask_router.get("/task/{task_id}", response_model=list[SubTaskRead],summary="Получить подзадачу по TaskID")
-def read_subtasks_TaskID(task_id: int, db: Session = Depends(get_db)):
-    return task_crud.get_subtasks_TaskID(db, task_id)
-
-
-
-''' Эндпоинт: Добавление подзадачи POST /tasks/'''
-@subtask_router.post("/", status_code=201,summary="Добавление подзадачи")
-def create_new_subtask(subtask: SubTaskCreate, db: Session = Depends(get_db)):
-    new_id = task_crud.create_subtask(db, subtask)
-    return {"message": "Подзадача успешно добавлена", "SubTaskID": new_id}
