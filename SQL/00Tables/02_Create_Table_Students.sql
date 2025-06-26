42565f3e6facd8aedc472f60363ffdcc541c844c
obeginin <obeginin@lihotec.com>
2025-06-26 21:20:29 +0300

    1

--- SQL/00Tables/02_Create_Table_Students.sql
+++ SQL/00Tables/02_Create_Table_Students.sql
@@ -22,20 +22,29 @@ BEGIN
 			BirthDate datetime,
 			Comment nvarchar(MAX),
 			Password VARCHAR(255),-- Хеш пароля (надо сделать NOT NULL)
-			Role nvarchar(15), -- роль (админс, user) СТАРОЕ УДАЛИТЬ!!!!
+			--Role nvarchar(15), -- роль (админс, user) СТАРОЕ УДАЛИТЬ!!!!
 			RoleID INT NOT NULL, -- id роли
 			FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)
 		);
 	END
 END;
 
+
+EXEC sp_help 'Students';
 --по умолчанию проставляем роль "Ученик"
 ALTER TABLE Students
 ADD CONSTRAINT DF_Students_RoleID DEFAULT 3 FOR RoleID;
-
+update Students set RoleID=3
 /*
+select * from students
+Добавить новый столбец
+ALTER TABLE Students ADD RoleID INT;
+Добавить внешний ключ по этому столбцу
+ALTER TABLE Students
+ADD CONSTRAINT FK_Students_RoleID FOREIGN KEY (RoleID) REFERENCES Roles(RoleID);
 
-
+ALTER TABLE Students
+ADD CONSTRAINT DF_Students_RoleID DEFAULT 3 FOR RoleID;
 DROP PROCEDURE Create_Table_Students
 
 drop table Students
@@ -53,3 +62,5 @@ INSERT INTO Students (Login, Last_Name, First_Name, Middle_Name, Email, Sex, Bir
 		('test', 'Тестов', 'Тест', 'Тестович', 'test@gmail.com', 'M', '', 'Комментарий','$pbkdf2-sha256$29000$N4bwvvceQ2gtBaD03htDyA$BLHTA0T4Q.f1kZMSLoQjTm1.Pr7hUMpK/dFKMtDdAkk','student');
 
 */
+INSERT INTO Students (Login, Last_Name, First_Name, Middle_Name, Email, Sex, BirthDate, Comment,Password,Role) 
+VALUES ('test1', 'Тестов', 'Тест', 'Тестович', 'test@gmail.com', 'M', '', 'Комментарий','$pbkdf2-sha256$29000$N4bwvvceQ2gtBaD03htDyA$BLHTA0T4Q.f1kZMSLoQjTm1.Pr7hUMpK/dFKMtDdAkk','student');
