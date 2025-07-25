ee73525830c88269a201233b75d3e36bd78a127a
Администратор <beginin-oleg@yandex.ru>
2025-07-25 12:03:15 +0300

    комментировал столбцы плюс меняю основную хранимку

--- Service/SQL/00Tables/04_Create_Table_SubTasks.sql
+++ Service/SQL/00Tables/04_Create_Table_SubTasks.sql
@@ -1,4 +1,4 @@
-﻿--������� ��������, ������� ������� ������� � ��������--
+﻿
 IF EXISTS(SELECT 1 FROM sys.procedures WHERE OBJECT_SCHEMA_NAME([object_id]) = 'dbo' and name = 'Create_Table_SubTasks')
 	DROP PROCEDURE dbo.Create_Table_SubTasks
 GO
@@ -9,16 +9,16 @@ BEGIN
 	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'SubTasks')
 	BEGIN
 		CREATE TABLE SubTasks (
-		SubTaskID INT IDENTITY(1,1) PRIMARY KEY, --id ��������� (��������� ����)
-		TaskID INT NOT NULL,  -- ����� � �������
-		SubTaskNumber INT NOT NULL,  -- ����� ��������� (1.1, 1.2, ...)
-		VariantID INT,
-		ImagePath NVARCHAR(255), --���� ������
-		Description NVARCHAR(MAX),  -- �������� ���������
-		Answer NVARCHAR(32), --�����
-		SolutionPath NVARCHAR(255), --������� ������
-		FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID) ON UPDATE CASCADE, -- ������� ����
-		FOREIGN KEY (VariantID) REFERENCES Variants(VariantID) ON UPDATE CASCADE
+		SubTaskID INT IDENTITY(1,1) PRIMARY KEY,				-- id задачи 
+		TaskID INT NOT NULL,									-- id категории
+		SubTaskNumber INT NOT NULL,								-- типы задач в данной категории (1.1, 1.2, ...)
+		VariantID INT,											-- id варианта
+		ImagePath NVARCHAR(255),								-- адрес с изображением задачи
+		Description NVARCHAR(MAX),								-- описание задачи
+		Answer NVARCHAR(32),									-- ответ задачи (НАДО ПЕРЕНСТИ В ОТДЕЛЬНУЮ ТАБЛИЦУ)
+		SolutionPath NVARCHAR(255),								-- адрес с решением задачи
+		FOREIGN KEY (TaskID) REFERENCES Tasks(TaskID) ON UPDATE CASCADE,		 -- внешний ключ на id категории
+		FOREIGN KEY (VariantID) REFERENCES Variants(VariantID) ON UPDATE CASCADE -- внешний ключ на id варианта
 		);
 	END
 END;
