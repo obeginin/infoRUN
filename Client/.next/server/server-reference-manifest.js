f2957d228fd8abb8ede96ec8a07681d39c1614e6
Администратор <beginin-oleg@yandex.ru>
2025-08-20 17:32:14 +0300

    рабочий билд

--- Client/.next/server/server-reference-manifest.js
+++ Client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wV/Vf4fRb/fn/2zyMwDPSGtfxDDxLdRouyMPLT9sBco=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
