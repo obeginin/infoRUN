e7d744b1a228abffbcc8224f3d565e40316e0004
Администратор <beginin-oleg@yandex.ru>
2025-09-02 12:54:27 +0300

    new branch backen/back

--- Service/config_app.py
+++ Service/config_app.py
@@ -1,15 +1,3 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-
-
-
-'''подключаем токен из конфига .env'''
-#SECRET_KEY = os.getenv("SECRET_KEY")
-#ALGORITHM = os.getenv("ALGORITHM", "HS256")  # второе значение дефолт
-#ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 300))
-
 
 
 
