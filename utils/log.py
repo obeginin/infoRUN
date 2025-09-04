82e2cd1c293bb0efa8160a9473a68981272dcd25
Администратор <beginin-oleg@yandex.ru>
2025-09-04 17:34:18 +0300

    postgres part 1

--- utils/log.py
+++ utils/log.py
@@ -9,6 +9,31 @@ import shutil
 import re
 from pathlib import Path
 
+
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+import logging
+import time
+logger = logging.getLogger("requests")
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start = time.time()
+        client_ip = request.client.host if request.client else "unknown"
+        logger.info(f"[НАЧАЛО] {request.method} {request.url.path} от {client_ip}")
+
+        try:
+            response = await call_next(request)
+        except Exception as e:
+            duration = time.time() - start
+            logger.exception(
+                f"[ОШИБКА] {request.method} {request.url.path} от {client_ip} - исключение: {e} - время: {duration:.3f} сек")
+            raise  # обязательно пробрасываем ошибку дальше, чтобы FastAPI мог её корректно обработать
+        else:
+            duration = time.time() - start
+            logger.info(
+                f"[ЗАВЕРШЕНО] {request.method} {request.url.path} - статус {response.status_code} - время: {duration:.3f} сек")
+            return response
 def setup_logging(log_file: str = "app.log", archive_dir: Path = None):
     log_dir = settings.LOG_DIR
     log_path = log_dir / log_file
